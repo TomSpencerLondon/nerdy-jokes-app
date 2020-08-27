f03a1bfe7e0365101018eef75e5b8e1397f85514
Andrei Bogomja <bogomja@gmail.com>
2020-08-27 22:32:46 +0100

    Modifies installation script

--- kind/install.sh
+++ kind/install.sh
@@ -2,19 +2,19 @@
 
 kind create cluster --config=kind-config.yml
 
-kubectl apply -f ./ingress/nginx-ingress.yml
+kubectl apply -f ./ingress-nginx/controller.yml
+kubectl apply -f ./ingress-nginx/metrics-svc.yml
 kubectl wait --namespace ingress-nginx \
     --for=condition=ready pod \
     --selector=app.kubernetes.io/component=controller \
     --timeout=90s
-
-kubectl create namespace dev
 
 kubectl apply -f ./prometheus/prometheus-operator-bundle.yml
 kubectl apply -f ./prometheus/prometheus-instance.yml
 kubectl apply -f ./prometheus/prometheus-servicemonitor.yml
 kubectl apply -f ./prometheus/prometheus-ingress.yml
 
+kubectl apply -f ./ingress-nginx/servicemonitor.yml
+
 kubectl apply -f ./app/nerdy-jokes-app.yml
 kubectl apply -f ./app/nerdy-jokes-app-servicemonitor.yml
-
