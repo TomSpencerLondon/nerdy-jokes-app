6df76dc8f0c109590f54b6b9c07cb8195fe2f197
Andrei Bogomja <bogomja@gmail.com>
2020-08-29 22:01:53 +0100

    Adds configuration for grafana deployment

--- kind/install.sh
+++ kind/install.sh
@@ -19,3 +19,10 @@ kubectl apply -f ./ingress-nginx/servicemonitor.yml
 kubectl apply -f ./nerdy-jokes-app/deployment.yml
 kubectl apply -f ./nerdy-jokes-app/servicemonitor.yml
 kubectl apply -f ./nerdy-jokes-app/ingress.yml
+
+kubectl apply -f ./grafana/crds -n monitoring
+kubectl apply -f ./grafana/roles -n monitoring
+kubectl apply -f ./grafana/operator.yml -n monitoring
+kubectl apply -f ./grafana/instance.yml -n monitoring
+kubectl apply -f ./grafana/prometheus-datasource.yml -n monitoring
+kubectl apply -f ./grafana/servicemonitor.yml -n monitoring
